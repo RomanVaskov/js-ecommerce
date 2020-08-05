b45c12e43aab2af72daab90aef62fb1523467627
RomanVaskov <romavaskov@gmail.com>
2020-08-05 18:13:12 +0300

    Add To Cart Action

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,14 @@
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
 import Error404Screen from './screens/Error404Screen'
+import CartScreen from './screens/CartScreen'
 import {parseRequestUrl} from './utils'
 
 const routes = {
   '/': HomeScreen,
   '/product/:id': ProductScreen,
+  '/cart/:id': CartScreen,
+  '/cart': CartScreen,
 }
 
 const router = async () => {
@@ -17,6 +20,7 @@ const router = async () => {
   const screen = routes[parsedUrl] ? routes[parsedUrl] : Error404Screen
   const main = document.getElementById('main_container')
   main.innerHTML = await screen.render()
+  await screen.after_render()
 }
 window.addEventListener('load', router)
 window.addEventListener('hashchange', router)
