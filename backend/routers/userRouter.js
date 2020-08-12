de135b6340d3dc074529ab2821e51120d29392ac
RomanVaskov <romavaskov@gmail.com>
2020-08-12 12:29:33 +0300

    Sign-in backend

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -1,20 +1,49 @@
 import express from 'express'
+import expressAsyncHandler from 'express-async-handler'
 import User from '../models/userModel'
+import {generateToken} from '../utils'
 
 const userRouter = express.Router()
 
-userRouter.get('/createadmin', async (req, res) => {
-  try {
-    const user = new User({
-      name: 'admin',
-      email: 'admin@example.com',
-      password: 'jsamazona',
-      isAdmin: true,
+userRouter.get(
+  '/createadmin',
+  expressAsyncHandler(async (req, res) => {
+    try {
+      const user = new User({
+        name: 'admin',
+        email: 'admin@example.com',
+        password: 'jsamazona',
+        isAdmin: true,
+      })
+      const createUser = await user.save()
+      res.send(createUser)
+    } catch (error) {
+      res.status(500).send({message: error.message})
+    }
+  })
+)
+
+userRouter.post(
+  '/signin',
+  expressAsyncHandler(async (req, res) => {
+    const signinUser = await User.findOne({
+      email: req.body.email,
+      password: req.body.password,
     })
-    const createUser = await user.save()
-    res.send(createUser)
-  } catch (error) {
-    res.status(500).send({message: error.message})
-  }
-})
+    if (!signinUser) {
+      res.status(401).send({
+        message: 'Invalid Email or Password',
+      })
+    } else {
+      res.send({
+        _id: signinUser._id,
+        name: signinUser.name,
+        email: signinUser.email,
+        isAdmin: signinUser.isAdmin,
+        token: generateToken(signinUser),
+      })
+    }
+  })
+)
+
 export default userRouter
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import bodyParser from 'body-parser'
 import data from './data'
 import config from './config'
 import userRouter from './routers/userRouter'
@@ -21,7 +22,7 @@ mongoose
 const app = express()
 
 app.use(cors())
-
+app.use(bodyParser.json())
 app.use('/api/users', userRouter)
 
 app.get('/api/products', (req, res) => {
@@ -36,7 +37,10 @@ app.get('/api/products/:id', (req, res) => {
     res.status(404).send({message: 'Product Not Found!'})
   }
 })
-
+app.use((err, req, res, next) => {
+  const status = err.name && err.name === 'ValidationError' ? 400 : 500
+  res.status(status).send({message: err.message})
+})
 app.listen(5000, () => {
   console.log('serve at http://localhost:5000')
 })
